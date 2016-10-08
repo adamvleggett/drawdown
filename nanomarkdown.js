8519cae3be0ae05c6fd934bc0e8a81157a39ecf5
Adam Leggett <adamvleggett@gmail.com>
2016-10-08 13:39:13 -0400

    Fix support for several edge cases; performance improvements; size reduction

--- nanomarkdown.js
+++ nanomarkdown.js
@@ -6,37 +6,45 @@
 
 ;function markdown(src) {
 
-    var r;
-    var store = [];
+    function highlight(src, done) {
+        return src.replace(/((\*\*?|__?)|~~)(\S.*?\S)\1/g, function(p0, p1, p2, p3) {
+            var rep = "sib".charAt(p2 ? p2.length : 0);
+            return "<" + rep + ">" + (done ? p3 : highlight(p3,1)) + "</" + rep + ">";
+        });
+    }
 
-    src = ("\n" + src
+    var stash = [];
+
+    return highlight(("\n" + src
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/\t/g, "  ")
         .replace(/\r/g, "") + "\n\n")
 
     // list
-    .replace(/^((\s*)([*-]|[\da-zA-Z][.)]) [^\n]+\n)+/gm, function(p0, p1, p2) {
+    .replace(/^((\s*)([*\-+]|[\da-zA-Z][.)]) .+?\n)+/gm, function(p0, p1, p2) {
         var rep = "";
         var lines = p0.split("\n");
         var close = [];
         var stat = -1;
         var indent = 0;
         for (var i = 0; i < lines.length; i++) {
-            if ((line = /^(\s*)(([*-])|(\d[.)])|([a-z][.)])|([A-Z][.)])) ([^\n]+)/.exec(lines[i]))) {
+            if ((line = /^(\s*)(([*\-+])|(\d[.)])|([a-z][.)])|([A-Z][.)])) ([^\n]+)/.exec(lines[i]))) {
                 var nstat = line[1].length - p2.length >> 1;
                 while (stat > nstat) {
                     rep += close.pop();
                     stat--;
                 }
                 while (stat < nstat) {
-                    var tag = line[3] ? ["<ul>","</ul>"]
-                            : line[4] ? ["<ol>","</ol>"]
-                            : line[6] ? ["<ol style='list-style-type:upper-alpha'>","</ol>"]
-                            :           ["<ol style='list-style-type:lower-alpha'>","</ol>"];
-                     rep += tag[0];
-                     close.push(tag[1]);
-                     stat++;
+                    rep += line[3]
+                        ? "<ul>"
+                        : "<ol" + (line[4]
+                            ? ">"
+                            : " style='list-style-type:" + (line[6]
+                                ? "upper-alpha'>"
+                                : "lower-alpha'>"));
+                    close.push(line[3] ? "</ul>" : "</ol>");
+                    stat++;
                 }
                 rep += "<li>" + line[7] + "</li>";
             }
@@ -46,35 +54,31 @@
 
     // code
     .replace(/\n((```|~~~).*\n?((.|\n)*?)\2|((    .*?\n)+))/g, function(p0, p1, p2, p3, p4, p5) {
-        store.push("<code>" + (p3||p5.replace(/^    /gm, "")) + "</code>");
-        return "<" + (store.length-1) + ">";
+        stash.push("<code>" + (p3||p5.replace(/^    /gm, "")) + "</code>");
+        return "<" + (stash.length-1) + ">";
     })
 
     // link
     .replace(/!?\[([^\]<>]+)\]\(([^ \)<>]+)( "[^\(\)\"]+")?\)/g, function(p0, p1, p2) {
-        store.push(/^!/.test(p0)
+        stash.push(/^!/.test(p0)
             ? "<img src='" + p2 + "' alt='" + p1 + "'/>"
             : p1.link(p2));
-        return "<" + (store.length-1) + ">";
+        return "<" + (stash.length-1) + ">";
     })
 
     // heading
-    .replace(/\n(#{1,6}) (.*?)( \1)?(\n|$)/g, function(p0, p1, p2) {
+    .replace(/^(#{1,6}) (.*?)( \1)?\s*$/gm, function(p0, p1, p2) {
         var cnt = p1.length;
         return "\n\n<h" + cnt + ">" + p2 + "</h" + cnt + ">\n";
     })
 
     // horizontal rule
-    .replace(/^(?:([*-_] ?)+)\1\1$/gm, "\n<hr/>\n");
+    .replace(/^(?:([*\-=_] ?)+)\1\1\s*$/gm, "\n<hr/>\n"))
 
-    // bold/italic/strikethrough
-    while ((r = /(^|\B)(?:(\*{1,2}|_{1,2}|~~))(.*?)\2($|\B)/g.exec(src))) {
-        var rep = "sib".charAt(r[2] == "~~" ? 0 : r[2].length);
-        src = src.replace(r[0], "<" + rep + ">" + r[3] + "</" + rep + ">");
-    }
+    // stash
+    .replace(/<(\d+)>/g, function(p0, p1) {
+        return stash[p1];
+    })
 
-    for (var i = 0; i < store.length; i++)
-        src = src.replace("<" + i + ">", store[i]);
-
-    return src.replace(/((.|\n)*?)\n\s*\n/g, "<p>$1</p>");
+    .replace(/((.|\n)*?)\n\s*\n/g, "<p>$1</p>");
 };
