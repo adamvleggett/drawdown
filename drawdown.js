6654323548c800ea7cc9b8ded3b99b99839492a3
Adam Leggett <adamleggett@interactivebrokers.com>
2020-10-26 12:05:01 -0400

    Table support; various minor fixes and tweaks

--- drawdown.js
+++ drawdown.js
@@ -6,96 +6,121 @@
 
 ;function markdown(src) {
 
+    var rx_lt = /</g;
+    var rx_gt = />/g;
+    var rx_space = /\t|\r|\uf8ff/g;
+    var rx_escape = /\\([\\\|`*_{}\[\]()#+\-~])/g;
+    var rx_hr = /^([*\-=_] *){3,}$/gm;
+    var rx_blockquote = /\n *&gt; *([^]*?)(?=(\n|$){2})/g;
+    var rx_list = /\n( *)(?:[*\-+]|((\d+)|([a-z])|[A-Z])[.)]) +([^]*?)(?=(\n|$){2})/g;
+    var rx_listjoin = /<\/(ol|ul)>\n\n<\1>/g;
+    var rx_highlight = /(^|[^A-Za-z\d\\])(([*_])|(~)|(\^)|(--)|(\+\+)|`)(\2?)([^<]*?)\2\8(?!\2)(?=\W|_|$)/g;
+    var rx_code = /\n((```|~~~).*\n?([^]*?)\n?\2|((    .*?\n)+))/g;
+    var rx_link = /((!?)\[(.*?)\]\((.*?)( ".*")?\)|\\([\\`*_{}\[\]()#+\-.!~]))/g;
+    var rx_table = /\n(( *\|.*?\| *\n)+)/g;
+    var rx_thead = /^.*\n( *\|( *\:?-+\:?-+\:? *\|)* *\n|)/;
+    var rx_row = /.*\n/g;
+    var rx_cell = /\||(.*?[^\\])\|/g;
+    var rx_heading = /(?=^|>|\n)([>\s]*?)(#{1,6}) (.*?)( #*)? *(?=\n|$)/g;
+    var rx_para = /(?=^|>|\n)\n+([^<]+?)\n+(?=\n|<|$)/g;
+    var rx_stash = /-\d+\uf8ff/g;
+
     function replace(rex, fn) {
         src = src.replace(rex, fn);
     }
 
     function element(tag, content) {
-        return "<" + tag + ">" + content + "</" + tag + ">";
+        return '<' + tag + '>' + content + '</' + tag + '>';
     }
 
     function blockquote(src) {
-        return src.replace(/\n *&gt; *([^]*?)(?=(\n|$){2})/g,
-        function(all, content) {
-            return element("blockquote", blockquote(content.replace(/^ *&gt; */gm,"")));
+        return src.replace(rx_blockquote, function(all, content) {
+            return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
         });
     }
 
     function list(src) {
-        return src.replace(/\n( *)([*\-+]|((\d+)|([a-z])|[A-Z])[.)]) +([^]*?)(?=(\n|$){2})/g,
-        function(all, ind, _, ol, num, low, content) {
-            var rind = "\n {0," + (ind.length+1) + "}";
-            var entry = content.split(
-                RegExp(rind + "(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +", "g")).map(list);
+        return src.replace(rx_list, function(all, ind, ol, num, low, content) {
+            var entry = element('li', highlight(content.split(
+                RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
 
-            return (ol
-                ? "<ol start='" + (num
-                    ? ol + "'>"
-                    : (parseInt(ol,36)-9) + "' style='list-style-type:" + (low ? "low" : "upp") + "er-alpha'>")
-                : "<ul>")
-                + element("li", highlight(entry.join("</li>\n<li>").replace(RegExp(rind,"g"),"\n")))
-                + (ol ? "</ol>" : "</ul>");
+            return '\n' + (ol
+                ? '<ol start="' + (num
+                    ? ol + '">'
+                    : parseInt(ol,36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
+                : element('ul', entry));
         });
     }
 
     function highlight(src) {
-        return src.replace(/(^|\W|_)(([*_])|(~)|`)(\2?)([^<]*?)\2\5(?!\2)(?=\W|_|$)/g,
-        function(all, _, p1, bi, ss, p2, content) {
+        return src.replace(rx_highlight, function(all, _, p1, emp, sub, sup, small, big, p2, content) {
             return _ + element(
-                    bi ? (p2 ? "b" : "i")
-                  : ss ? (p2 ? "s" : "sub") : "code",
+                  emp ? (p2 ? 'strong' : 'em')
+                : sub ? (p2 ? 's' : 'sub')
+                : sup ? 'sup'
+                : small ? 'small'
+                : big ? 'big'
+                : 'code',
                 highlight(content));
         });
     }
 
+    const unesc = str => str.replace(rx_escape, '$1');
+
     var stash = [];
     var si = 0;
 
-    src = "\n" + src + "\n";
+    src = '\n' + src + '\n';
 
-    replace(/</g, "&lt;");
-    replace(/>/g, "&gt;");
-    replace(/\t|\r/g, "  ");
+    replace(rx_lt, '&lt;');
+    replace(rx_gt, '&gt;');
+    replace(rx_space, '  ');
 
     // blockquote
     src = blockquote(src);
 
     // horizontal rule
-    replace(/^([*\-=_] *){3,}$/gm, "<hr/>");
+    replace(rx_hr, '<hr/>');
 
     // list
     src = list(src);
+    replace(rx_listjoin, '');
 
     // code
-    replace(/\n((```|~~~).*\n?([^]*?)\2|((    .*?\n)+))/g, function(all, p1, p2, p3, p4) {
-        stash[--si] = element("pre", element("code", p3||p4.replace(/^    /gm, "")));
-        return si + "\r";
+    replace(rx_code, function(all, p1, p2, p3, p4) {
+        stash[--si] = element('pre', element('code', p3||p4.replace(/^    /gm, '')));
+        return si + '\uf8ff';
     });
 
-    // link or escape
-    replace(/((!?)\[(.*?)\]\((.*?)( ".*")?\)|\\([\\`*_{}\[\]()#+\-.!~]))/g, function(all, p1, p2, p3, p4, p5, p6) {
+    // link or image
+    replace(rx_link, function(all, p1, p2, p3, p4, p5, p6) {
         stash[--si] = p4
-            ? (p2
-                ? "<img src='" + p4 + "' alt='" + p3 + "'/>"
-                : p3.link(p4))
+            ? p2
+                ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
+                : '<a' + (options.target ? ' target="' + options.target + '"' : '') + ' href="' + p4 + '">' + unesc(highlight(p3)) + '</a>'
             : p6;
-        return si + "\r";
+        return si + '\uf8ff';
+    });
+
+    // table
+    replace(rx_table, (all, table) => {
+        const sep = table.match(rx_thead)[1];
+        return '\n' + element('table',
+            table.replace(rx_row, (row, ri) =>
+                row == sep ? '' : element('tr', row.replace(rx_cell, (all, cell, ci) =>
+                    ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''))
+            )
+        )
     });
 
     // heading
-    replace(/(?=^|>|\n)([>\s]*?)(#{1,6}) (.*?)( #*)? *(?=\n|$)/g, function(all, _, p1, p2) {
-        return _ + element("h" + p1.length, highlight(p2));
-    });
+    replace(rx_heading, (all, _, p1, p2) => _ + element('h' + p1.length, unesc(highlight(p2))));
 
     // paragraph
-    replace(/(?=^|>|\n)\n+([^<]+?)\n+(?=\n|<|$)/g, function(all, c) {
-        return element("p", highlight(c));
-    });
+    replace(rx_para, (all, content) => element('p', unesc(highlight(content))));
 
     // stash
-    replace(/-\d+\r/g, function(all) {
-        return stash[+all];
-    });
+    replace(rx_stash, all => stash[parseInt(all)]);
 
-    return src;
+    return src.trim();
 };
